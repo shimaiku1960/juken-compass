542e123f1cf369b5a398459a44c84493fa48a64e
shimaiku1960 <dikurou@gmail.com>
2026-06-05 20:37:59 +0900

    面接練習を踏まえて少し機能を追加した気がする

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";                                                                                                                
-  import { createClient } from "@/lib/supabase/server";                                                                                                      
+import { createClient } from "@/lib/supabase/server";
+import prisma from "@/lib/prisma";     
+
                                                                                                                                                              
   export const GET = async (request: Request) => {
     const { searchParams, origin } = new URL(request.url);                                                                                                   
@@ -9,7 +11,25 @@ import { NextResponse } from "next/server";
       const supabase = await createClient();                                                                                                               
       const { error } = await supabase.auth.exchangeCodeForSession(code);                                                                                    
    
-      if (!error) {                                                                                                                                          
+      if (!error) {    
+        const { data: { user } } = await supabase.auth.getUser();
+
+        if (user) {
+          const existingProfile = await prisma.profile.findUnique({
+            where: { id: user.id },
+          });
+
+          if (!existingProfile) {
+            await prisma.profile.create({
+              data: {
+                id: user.id,
+                nickname: user.email?.split("@")[0] ?? "名無し",
+              },
+            });
+          }
+        }
+
+
         return NextResponse.redirect(`${origin}/`);                                                                                                        
       }
     }                                                                                                                                                        
